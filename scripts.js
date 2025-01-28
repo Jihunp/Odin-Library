bb6cc28dda66e2057f4c7f1e0d535169cd873250
Edward Jihun Park <92244135+Jihunp@users.noreply.github.com>
2025-01-28 00:29:44 -0800

    setup for this project

---
+++ scripts.js
@@ -0,0 +1,29 @@
+const libary = 
+[
+  {
+    title: "dummy",
+    author: "dumb and duumber",
+    pages: "222",
+    read: false,
+    rating: 1,
+    id: 0
+  },
+  {
+    title: "coconut",
+    author: "moana",
+    pages: "111",
+    read: false,
+    rating: 1,
+    id: 1
+  },
+];
+
+
+
+function Book() {
+
+}
+
+function addBookToLibrary() {
+
+}
