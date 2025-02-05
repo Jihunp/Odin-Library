445620477a183e2a21d08c9006b43c5505af7b51
Edward Jihun Park <92244135+Jihunp@users.noreply.github.com>
2025-02-05 02:00:07 -0800

    displayed all dummy books from library

--- scripts.js
+++ scripts.js
@@ -29,40 +29,40 @@ function Book(title, author, pages, read, rating, id) {
   this.pages = pages;
   this.read = read;
   this.rating = rating;
-  this.id = library.length;
+  this.id = id;
 }
 
 // event handler
 function setDisplayLibrary() {
   displayButton.addEventListener("click", displayLibrary);
 }
+
 function displayLibrary() {
-
+  libDisplay.innerHTML = "";
+  library.forEach(book => {
+    const row =
+      `
+      <tr>
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.pages}</td>
+        <td>${book.read ? "Yes" : "No"}</td>
+        <td>${book.rating}</td>
+      </tr>
+      `;
+      libDisplay.innerHTML += row;
+  })
   console.log("coconuts")
-
 }
-
-//test document 
-
-// const libraryBody = document.getElementById(
-//   "library-display");
-//   libraryBody.innerHTML = "";
-
-// library.forEach(book => {
-//   const row = `<tr>
-//   <td>${book.title}</td>
-//   <td>${book.author}</td>
-//   <td>${book.pages}</td>
-//   <td>${book.read ? "Yes" : "No"}</td>
-//   <td>${book.rating}</td>
-// </tr>`;
-// tableBody.innerHTML += row;
-// });
 
 
 function addBookToLibrary() {
 
 }
 
+function editBook() {
+
+}
 
 
+setDisplayLibrary()
